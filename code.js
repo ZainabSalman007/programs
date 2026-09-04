bd3f8294e4bb1d9c0fb5915d4830096e8dfdf7c0
Zaimellow <134494831+ZainabSalman007@users.noreply.github.com>
2026-09-04 16:35:01 +0500

    Price Discount

--- code.js
+++ code.js
@@ -1,37 +1,36 @@
-const age = document.getElementById("age");
-const submitButton = document.getElementById("submit");
-const result = document.getElementById("result");
+const amount = document.getElementById('amount');
+const submit = document.getElementById('submit');
+const result = document.getElementById('result');
 
-function userAge(age) {
-   return age.value;
+function totalPrice(amount) {
+   return Number(amount.value);
 }
 
-function checkAge() {
-    let Age = userAge(age);
+function discountPrice() {
+    let price = totalPrice(amount)
+    let discount ;
+    let finalPrice;
 
-    if (Age > 0 && Age < 13) {
-      return "You are a kid.";
+    if (price >= 100 ){
+        discount = price * 0.20;
+        finalPrice = price - discount;
+        return `Your spent more than $100 so u get 20% discount and ur final price is ${finalPrice.toFixed(2)}`;
     }
 
-    else if (Age >= 13 && Age <= 17) {
-        return "You are a teenager.";
-    }
-
-    else if (Age >= 18 && Age < 60) {
-        return "You are an adult.";
-    }
-
-    else if (Age >= 60 && Age <= 120) {
-        return "You are a senior citizen.";
+    else if (price < 100 &&  price >= 50){
+        discount = price * 0.10;
+        finalPrice = price - discount;
+        return `Your spent more than $50 but less than $100 so u get 10% discount and ur final price is ${finalPrice.toFixed(2)}`;
     }
 
     else {
-        return "you do not exist Muhahahah .";
+        finalPrice= price;
+        return `Your spent less than $50 so u get no discount and ur final price is ${finalPrice.toFixed(2)}`;
     }
 }
 
-function displayResult() {
-    result.textContent = checkAge();
+function FinalPrice(){
+    result.textContent=discountPrice()
 }
 
-submitButton.addEventListener("click", displayResult);
+submit.addEventListener("click" , FinalPrice)
