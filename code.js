4673fe272fe690020143cc59f8aa00c3ac30a2b8
Zaimellow <134494831+ZainabSalman007@users.noreply.github.com>
2026-09-04 15:00:36 +0500

    password validator

--- code.js
+++ code.js
@@ -1,15 +1,18 @@
-const tempInput = document.getElementById("temperature");
-const result = document.getElementById("result");
+const password = document.getElementById("password");
 const submit = document.getElementById("submit");
+const result = document.getElementById("result");
 
-submit.onclick = function checkTemperature() {
-    const temperature = tempInput.value;
-    if (temperature > 30) {
-        result.textContent = "It's hot outside!";
-    } else if (temperature >= 15 && temperature <= 30) {
-        result.textContent = "The weather is pleasant.";
-    } else {
-        result.textContent = "It's cold outside.";
+submit.onclick = function() {
+    const passwordValue = password.value;
+    if (passwordValue.length < 8) {
+        result.textContent = "Password must be at least 8 characters long.";
+    }else if (!/[A-Z]/.test(passwordValue)) {
+        result.textContent = "Password must contain at least one uppercase letter.";
+    }else if (!/[a-z]/.test(passwordValue)) {
+        result.textContent = "Password must contain at least one lowercase letter.";
+    }else if (!/[0-9]/.test(passwordValue)) {
+        result.textContent = "Password must contain at least one number.";
+    }else {
+        result.textContent = "Password is valid.";
     }
 }
-    
