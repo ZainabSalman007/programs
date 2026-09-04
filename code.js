17df086893618d1d2f3b49713b1cfece0b19473c
Zaimellow <134494831+ZainabSalman007@users.noreply.github.com>
2026-09-04 17:12:19 +0500

    del pics

--- code.js
+++ code.js
@@ -1,32 +1,6 @@
-const bill = document.getElementById('bill');
-const submit = document.getElementById('submit');
-const result = document.getElementById('result');
+const prices = [1200, 500, 800, 300];
 
-function billAmount(bill) {
-   return Number(bill.value);
+function calculateTotal(prices){
+    l
 }
 
-function serviceCharges() {
-    let price = billAmount(bill);
-    let tax ;
-    let finalPrice;
-
-    if (price >= 50 ){
-        tax = price * 0.10;
-        finalPrice = price + tax;
-        return `Your spent more than $50 so u have to pay 10% service charges and ur final price is ${finalPrice.toFixed(2)}`;
-    }
-
-    else {
-        tax = price * 0.05;
-        finalPrice = price + tax;
-        return `Your spent less than $50 so u have to pay 5% service charges and ur final price is ${finalPrice.toFixed(2)}`;
-    }
-
-}
-
-function FinalPrice(){
-    result.textContent=serviceCharges()
-}
-
-submit.addEventListener("click" , FinalPrice)
