34cc3068b9abb744a0dc8af7c975fe5f2699f4e0
Zaimellow <134494831+ZainabSalman007@users.noreply.github.com>
2026-09-04 15:32:08 +0500

    age checker

--- code.js
+++ code.js
@@ -1,16 +1,23 @@
-function greetings(name) {
-    return "Hello, " + name + "!";
+const age = document.getElementById("age");
+const submitButton = document.getElementById("submit");
+const result = document.getElementById("result");
+
+function checkAge(age) {
+    return age ;
 }
 
-function displayGreeting() {
-    const nameInput = document.getElementById("greetings");
-    const resultParagraph = document.getElementById("result");
-    const name = nameInput.value;
-    const greetingMessage = greetings(name);
-    resultParagraph.textContent = greetingMessage;
+function displayMessage() {
+    if (checkAge(age.value) > 0 && checkAge(age.value) < 13) {
+        result.textContent = "You are a kid.";
+    }else if (checkAge(age.value) >= 13 && checkAge(age.value) <= 17) {
+        result.textContent = "You are a teenager.";
+    }else if (checkAge(age.value) >= 18 && checkAge(age.value) < 60) {
+        result.textContent = "You are an adult.";
+    }else if (checkAge(age.value) >= 60 && checkAge(age.value) <= 120) {
+        result.textContent = "You are a senior citizen.";
+    }else {
+        result.textContent = "you do not exist Muhahahah .";
+    }
 }
 
-const button = document.getElementById("submit");
-button.onclick = () => {
-    displayGreeting();
-};
+submitButton.addEventListener("click", displayMessage);
