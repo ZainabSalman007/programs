1e86e11cdb8346c6f92b595ff4d71f79191852cf
Zaimellow <134494831+ZainabSalman007@users.noreply.github.com>
2026-09-04 16:59:59 +0500

    service charges

--- code.js
+++ code.js
@@ -1,36 +1,32 @@
-const amount = document.getElementById('amount');
+const bill = document.getElementById('bill');
 const submit = document.getElementById('submit');
 const result = document.getElementById('result');
 
-function totalPrice(amount) {
-   return Number(amount.value);
+function billAmount(bill) {
+   return Number(bill.value);
 }
 
-function discountPrice() {
-    let price = totalPrice(amount)
-    let discount ;
+function serviceCharges() {
+    let price = billAmount(bill);
+    let tax ;
     let finalPrice;
 
-    if (price >= 100 ){
-        discount = price * 0.20;
-        finalPrice = price - discount;
-        return `Your spent more than $100 so u get 20% discount and ur final price is ${finalPrice.toFixed(2)}`;
-    }
-
-    else if (price < 100 &&  price >= 50){
-        discount = price * 0.10;
-        finalPrice = price - discount;
-        return `Your spent more than $50 but less than $100 so u get 10% discount and ur final price is ${finalPrice.toFixed(2)}`;
+    if (price >= 50 ){
+        tax = price * 0.10;
+        finalPrice = price + tax;
+        return `Your spent more than $50 so u have to pay 10% service charges and ur final price is ${finalPrice.toFixed(2)}`;
     }
 
     else {
-        finalPrice= price;
-        return `Your spent less than $50 so u get no discount and ur final price is ${finalPrice.toFixed(2)}`;
+        tax = price * 0.05;
+        finalPrice = price + tax;
+        return `Your spent less than $50 so u have to pay 5% service charges and ur final price is ${finalPrice.toFixed(2)}`;
     }
+
 }
 
 function FinalPrice(){
-    result.textContent=discountPrice()
+    result.textContent=serviceCharges()
 }
 
 submit.addEventListener("click" , FinalPrice)
