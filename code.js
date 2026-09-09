bc519fd1c99518b0097f2ab4bc3c130a5fe441b3
Zaimellow <134494831+ZainabSalman007@users.noreply.github.com>
2026-09-10 00:54:03 +0500

    calculator

--- code.js
+++ code.js
@@ -1,46 +1,19 @@
 const display = document.getElementById("display");
 
-let timer = null;
-let elapsedTime = 0;
-let startTime = 0;
+function work(input){
+    display.value += input;
+}
 
-function start(){
-    if(timer === null){
-        startTime = Date.now() - elapsedTime;
-        timer = setInterval(updateDisplay , 10) ;
+function erase(){
+    display.value = "";
+}
 
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "ERROR";
     }
 }
 
-function stop(){
-    if(timer !== null){
-        clearInterval(timer);
-        timer = null;
-        elapsedTime = Date.now() - startTime;
-    }
-}
-
-function reset(){
-    clearInterval();
-    timer = null;
-    startTime = 0;
-    elapsedTime = 0;
-
-    display.textContent = "00:00:00:00"
-}
-
-function updateDisplay(){
-    elapsedTime =  Date.now() - startTime;
-
-    let hours = Math.floor(elapsedTime / (1000 * 60 *60));
-    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
-    let seconds = Math.floor(elapsedTime / 1000 % 60);
-    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
-
-    hours = String(hours).padStart(2 , "0");
-    minutes = String(minutes).padStart(2 , "0");
-    seconds = String(seconds).padStart(2 , "0");
-    milliseconds = String(milliseconds).padStart(2 , "0");
-
-    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}: `
-}
