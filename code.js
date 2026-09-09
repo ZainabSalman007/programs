bcf17df1c48403a78621e446fe7a90cb45cf1ed5
Zaimellow <134494831+ZainabSalman007@users.noreply.github.com>
2026-09-09 14:58:31 +0500

    Stopwatch

--- code.js
+++ code.js
@@ -1,14 +1,46 @@
-function updateClock(){
-    const now = new Date();
-    let hours = now.getHours();
-    const meridiem = hours >=12 ? "PM" : "AM" ; 
-    hours = hours % 12 || 12 ;
-    hours = hours.toString().padStart(2 , 0);
-    const minutes = now.getMinutes().toString().padStart(2 , 0);
-    const seconds = now.getSeconds().toString().padStart(2 , 0);
-    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
-    document.getElementById("clock").textContent = timeString;
+const display = document.getElementById("display");
+
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+
+function start() {
+    if (timer === null) {
+        startTime = Date.now() - elapsedTime;
+
+        timer = setInterval(updateDisplay, 10);
+    }
 }
 
-updateClock();
-setInterval(updateClock , 1000)
+function stop() {
+    if (timer !== null) {
+        clearInterval(timer);
+        timer = null;
+        elapsedTime = Date.now() - startTime;
+    }
+}
+
+function reset() {
+    clearInterval(timer);
+    timer = null;
+    startTime = 0;
+    elapsedTime = 0;
+
+    display.textContent = "00:00:00:00";
+}
+
+function updateDisplay() {
+    elapsedTime = Date.now() - startTime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+}
