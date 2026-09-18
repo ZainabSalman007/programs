a5e482e6bec5d7c7b09af0159564dc5567516ead
Zaimellow <134494831+ZainabSalman007@users.noreply.github.com>
2026-09-18 15:30:34 +0500

    rock paper scissors

--- code.js
+++ code.js
@@ -1,25 +1,83 @@
-fetchData();
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissor = document.getElementById('scissor');
 
-async function fetchData(){
-    
-    try{
-        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+const player = document.getElementById("player");
+const comp = document.getElementById("comp");
+const result = document.getElementById("result");
+const score = document.getElementById("score");
 
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+let playerChoice;
+let compChoice;
+let choices = ["rock" , "paper" , "scissor"];
 
-        if(!response.ok){
-            throw new Error("coulnt fetch")
-        }
+let compScore = 0;
+let playerScore = 0;
 
-        const data = await response.json();
-        const pokemonSprite = data.sprites.front_default;
-        const imgElement = document.getElementById(`pokemonSprite`);
+rock.addEventListener("click" , ()=>{
+    playerChoice = "rock";
 
-        imgElement.src = pokemonSprite;
-        imgElement.style.display = 'block';
+    compChoice = choices[Math.floor(Math.random() * 3)];
+
+    player.textContent = `You chose: ${playerChoice}`;
+    comp.textContent = `Computer chose: ${compChoice}`;
+
+    if(compChoice == "paper"){
+        result.textContent = "comp won"
+        compScore++;
+    }
+    else if(compChoice == "scissor"){
+        result.textContent = "you won"
+        playerScore++;
+    }else{
+        result.textContent = "its a tie"
     }
 
-    catch(error){
-        console.error(error);
+    score.textContent = `You: ${playerScore} | Computer: ${compScore}`;
+})
+
+paper.addEventListener("click" , ()=>{
+    playerChoice = "paper";
+
+    compChoice = choices[Math.floor(Math.random() * 3)];
+
+    player.textContent = `You chose: ${playerChoice}`;
+    comp.textContent = `Computer chose: ${compChoice}`;
+
+     if(compChoice == "scissor"){
+        result.textContent = "comp won"
+        compScore++;
     }
-}
+    else if(compChoice == "rock"){
+        result.textContent = "you won"
+        playerScore++;
+    }else{
+        result.textContent = "its a tie"
+    }
+
+    score.textContent = `You: ${playerScore} | Computer: ${compScore}`;
+})
+
+scissor.addEventListener("click" , ()=>{
+    playerChoice = "scissor";
+
+    compChoice = choices[Math.floor(Math.random() * 3)];
+
+    player.textContent = `You chose: ${playerChoice}`;
+    comp.textContent = `Computer chose: ${compChoice}`;
+
+     if(compChoice == "rock"){
+        result.textContent = "comp won"
+        compScore++;
+    }
+    else if(compChoice == "paper"){
+        result.textContent = "you won"
+        playerScore++;
+    }else{
+        result.textContent = "its a tie"
+    }
+
+    score.textContent = `You: ${playerScore} | Computer: ${compScore}`;
+})
+
+
